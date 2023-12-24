d738b6e35f937e8d94518ca515a430d38b527736
Rayork <42216424+Rayork@users.noreply.github.com>
2023-12-24 06:28:34 +0100

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,30 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+exports.ox_target:addGlobalPlayer({
+    {
+        name = 'id',
+        icon = 'fa-solid fa-user',
+        label = 'ID',
+        onSelect = function(data)
+            local targetEntity = data.entity
+            
+            if DoesEntityExist(targetEntity) and IsEntityAPed(targetEntity) then
+                local playerId = GetPlayerServerId(NetworkGetEntityOwner(targetEntity))
+                ESX.ShowNotification(string.format("Targeted Player ID: %d", playerId))
+            else
+                print("No player targeted.")
+            end
+        end
+    },
+    {
+        name = 'porter',
+        icon = 'fa-solid fa-user',
+        label = 'Porter',
+        onSelect = function(data)
+            ExecuteCommand('porter')
+        end
+    },
+})
+
---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Rayork'
+description 'Script ID for quest'
+version '1.0.0'
+
+lua54 'yes'
+
+client_scripts {
+    'client.lua',
+}
